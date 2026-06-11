1bf9318ba8b593aced87cc25165dc6d370dea2ea
Barbara Ribeiro <barbara.lourenco@escola.pr.gov.br>
2026-06-10 21:02:52 -0300

    TESTE

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
-        for(let j = 0; j < botoes.length; j++) {
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
@@ -33,21 +33,26 @@ function calculaTempo(tempoObjetivo) {
     horas %= 24;
 
     if (tempoFinal > 0) {
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos.";
+        return [dias,horas,minutos,segundos];
     } else {
-        return "Prazo finalizado.";
+        return [0,0,0,0];
     }
 }
 
 function atualizarCronometro() {
-    for (let i=0; i < contadores.length; i++) {
-        contadores[i].textContent = calculaTempo(tempos[i]);
+    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+
+    for (let i = 0; i < contadores.length; i++) {
+        //contadores[i].textContent = calculaTempo(tempos[i]);
     }
 }
 
 function comecaCronometro() {
     atualizarCronometro();
-    setInterval(atualizarCronometro,1000);
+    setInterval(atualizarCronometro, 1000);
 }
 
 comecaCronometro();
