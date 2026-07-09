64ac5703fd9ded0f0e7715636c3d3dbe3c1668ab
Barbara Ribeiro <barbara.lourenco@escola.pr.gov.br>
2026-07-09 12:31:04 -0300

    09/07

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
-        for (let j = 0; j < botoes.length; j++) {
+        for(let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
@@ -13,7 +13,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-06-30T00:00:00");
+const tempoObjetivo1 = new Date("2026-07-10T00:00:00");
 const tempoObjetivo2 = new Date("2026-08-31T00:00:00");
 const tempoObjetivo3 = new Date("2026-07-31T00:00:00");
 const tempoObjetivo4 = new Date("2026-12-11T00:00:00");
@@ -41,7 +41,7 @@ function calculaTempo(tempoObjetivo) {
     }
 }
 
-function atualizarCronometro() {
+function calculaSituacao(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal / 1000);
@@ -52,7 +52,7 @@ function atualizarCronometro() {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-
+    
     if (tempoFinal > 0) {
         return "Ainda há tempo!";
     } else {
@@ -60,27 +60,29 @@ function atualizarCronometro() {
     }
 }
 
-function atualizaeSituacao(){
-    for(let i=0; i < situacoes.length; i++){
-        situacoes[i].textContent = calculaSituacao(tempo[i]);
+function atualizaSituacao() {
+    for( let i=0; i < situacoes.length; i++) {
+        situacoes[i].textContent = calculaSituacao(tempos[i]);
     }
 }
 
 function atualizarCronometro() {
-    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
-    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
-    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+    
 
-    for (let i = 0; i < contadores.length; i++) {
-        //contadores[i].textContent = calculaTempo(tempos[i]);
+    for (let i=0; i < contadores.length; i++) {
+        let tempo = calculaTempo(tempos[i]);
+
+        document.getElementById("dias"+i).textContent = tempo[0];
+        document.getElementById("horas"+i).textContent = tempo[1];
+        document.getElementById("min"+i).textContent = tempo[2];
+        document.getElementById("seg"+i).textContent = tempo[3];
     }
+    atualizaSituacao();
 }
 
 function comecaCronometro() {
     atualizarCronometro();
-    setInterval(atualizarCronometro, 1000);
+    setInterval(atualizarCronometro,1000);
 }
 
 comecaCronometro();
-atualizaeSituacao();
